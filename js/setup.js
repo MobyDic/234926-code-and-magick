e232a73ef9e8544942225dc03e41b252eb31960a
Yaroslav Makarenko <mobydic@mail.ru>
2017-01-30 01:26:37 +0300

    Правки по функции выбора случайного цвета

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,11 @@
 var setupOpen = document.querySelector('.setup-open');
 var overlay = document.querySelector('.overlay');
-var setupClose = document.querySelector('.setup-close');
-var nameField = document.querySelector('.setup-user-name');
-var wizardCoat = document.querySelector('#wizard-coat');
-var wizardEyes = document.querySelector('#wizard-eyes');
-var fireball = document.querySelector('.setup-fireball-wrap');
+var setupClose = overlay.querySelector('.setup-close');
+var nameField = overlay.querySelector('.setup-user-name');
+var wizard = overlay.querySelector('#wizard');
+var wizardCoat = wizard.querySelector('#wizard-coat');
+var wizardEyes = wizard.querySelector('#wizard-eyes');
+var fireball = overlay.querySelector('.setup-fireball-wrap');
 
 var coatColor = ['rgb(101, 137, 164)','rgb(241, 43, 107)','rgb(146, 100, 161)','rgb(56, 159, 117)','rgb(215, 210, 55)','rgb(0, 0, 0)'];
 var eyesColor = ['black','red','blue','yellow','green'];
@@ -12,32 +13,28 @@ var fireballColor = ['#ee4830','#30a8ee','#5ce6c0','#e848d5','#e6e848'];
 
 setupOpen.addEventListener('click', function() {
     overlay.classList.remove('invisible');
-  }
-);
+});
 
 setupClose.addEventListener('click', function() {
     overlay.classList.add('invisible');
-  }
-);
+});
 
 nameField.required = true;
 nameField.maxLength = 50;
 
 wizardCoat.addEventListener('click',function() {
-    wizardCoat.style.fill = coatColor[randomColor(coatColor.length)];
-  }
-);
+    wizardCoat.style.fill = randomColor(coatColor);
+});
 
 wizardEyes.addEventListener('click',function() {
-    wizardEyes.style.fill = coatColor[randomColor(eyesColor.length)];
-  }
-);
+    wizardEyes.style.fill = randomColor(eyesColor);
+});
 
 fireball.addEventListener('click',function() {
-    fireball.style.background = fireballColor[randomColor(eyesColor.length)];
-  }
-);
+    fireball.style.background = randomColor(eyesColor)s;
+});
 
-function randomColor( lengthArr) {
-  return Math.floor(Math.random() * lengthArr);
+function randomColor(Arr) {
+  var color = Arr[ Math.floor(Math.random() * Arr.length) ];
+  return color;
 }
